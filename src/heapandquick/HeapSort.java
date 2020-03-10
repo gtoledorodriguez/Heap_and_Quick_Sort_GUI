7d1e2d8b02bd8d8332f1b1ae1260a0c9f2587c8a
180138Ga <grace.m.arnold@outlook.com>
2020-03-10 11:25:09 -0500

    updating heapsort for testing

--- src/heapandquick/HeapSort.java
+++ src/heapandquick/HeapSort.java
@@ -1,44 +1,54 @@
 package heapandquick;
 
 public class HeapSort {
-	int left(int i) {
+	static int left(int i) {
 		return 2 * i;
 	}
-	int right(int i) {
+	static int right(int i) {
 		return 2 * i + 1;
 	}
-	void max_heapify (int[] arr, int i) {
+	static void max_heapify (int[] arr, int i, int heap_size) {
 		int largest;
 		int l = left(i);
 		int r = right(i);
-		if (l <= arr.length && arr[l] > arr[i]) {
+		if (l < heap_size && arr[l] > arr[i]) {
 			largest = l;
 		}
 		else {
 			largest = i;
 		}
-		if (r <= arr.length && arr[l] > arr[largest]) {
+		if (r < heap_size && arr[r] > arr[largest]) {
 			largest = r;
 		}
 		if (largest != i) {
 			int temp = arr[i];
 			arr[i] = arr[largest];
-		arr[largest] = temp;
-			max_heapify(arr, largest);
+			arr[largest] = temp;
+			heap_size = heap_size + 1;
+			max_heapify(arr, largest, heap_size);
 		}
 	}
-	void build_max_heap (int[] arr){
+	static void build_max_heap (int[] arr){
+		int heap_size = arr.length;
 		for (int i = arr.length / 2; i > 0; i--) {
-			max_heapify(arr, i);
+			max_heapify(arr, i, heap_size);
 		}
 	}
-	void heapsort(int[] arr) {
+	static void heapsort(int[] arr) {
 		build_max_heap(arr);
-		for (int i = arr.length; i < 1; i--) {
-			int temp = arr[0];
-			arr[0] = arr[i];
-			arr[i] = temp;
-			max_heapify(arr, 1);
+		for (int i = arr.length-1; i > 1; i--) {
+			int temp = arr[1];
+			arr[1] = arr[i];
+			arr[1] = temp;
+			max_heapify(arr, 1, 0);
+		}
+	}
+	public static void main(String[] args) {
+		int[] heap = {16, 14, 10, 8, 7, 9, 3, 2, 4, 1};
+		int[] heap2 = {3, 5, 2, 6, 1, 4};
+		heapsort(heap);
+		for (int i = 0; i < heap.length; i++) {
+			System.out.print(heap[i] + " ");
 		}
 	}
  }
